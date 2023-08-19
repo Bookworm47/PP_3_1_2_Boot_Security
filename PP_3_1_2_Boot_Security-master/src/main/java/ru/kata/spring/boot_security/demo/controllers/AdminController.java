edb23a72df9715020b7a0e6fa98dbac0cb553f26
Bookworm47 <konst.samsonoff@yandex.ru>
2023-08-19 15:54:48 +0300

    Initial commit

---
+++ PP_3_1_2_Boot_Security-master/src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -0,0 +1,62 @@
+package ru.kata.spring.boot_security.demo.controllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final UserService userService;
+    @Autowired
+    public AdminController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String showAllUsers(Model model) {
+        model.addAttribute("allUsers", userService.allUsers());
+        return ("allUsers");
+    }
+
+    @GetMapping("/newUser")
+    public String newUser(@ModelAttribute("user") User user) {
+        return "addUser";
+    }
+
+    @PostMapping("/")
+    public String addUser(@ModelAttribute("user") User user) {
+        userService.addUser(user);
+        return "redirect:/";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(Model model, @PathVariable("id") Long id) {
+        model.addAttribute("user", userService.getUser(id));
+        return "edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("user") User user) {
+        userService.updateUser(user);
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        userService.deleteUser(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/user")
+    public String shUser() {
+        return ("user");
+    }
+
+}
---
+++ PP_3_1_2_Boot_Security-master/src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceAuth.java
@@ -0,0 +1,52 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.model.Role;
+
+import ru.kata.spring.boot_security.demo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
+import ru.kata.spring.boot_security.demo.repositories.UserRepository;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Service
+public class UserServiceAuth implements UserDetailsService {
+
+    private final UserRepository userRepository;
+//    private final RoleRepository roleRepository;
+
+    @Autowired
+    public UserServiceAuth(UserRepository userRepository, RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+//        this.roleRepository = roleRepository;
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = findByUsername(username);
+        if(user == null) {
+            throw new UsernameNotFoundException(String.format("User '%s' not found", username));
+        }
+
+        return user;
+    }
+
+//    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+//        return roles.stream().map(r -> new SimpleGrantedAuthority(r.getName())).collect(Collectors.toList());
+//    }
+}
